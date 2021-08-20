d03ffa7b8e5fdbb5d516f416b3924e5b1e596cdd
Marco R <mprizzuto88@gmail.com>
2021-08-20 14:17:35 -0400

    create student list

---
+++ student-data.php
@@ -0,0 +1,17 @@
+<?php 
+$studentList = [
+	"name" => $name,
+	"slogan" => $slogan,
+	"link" => $link,
+];
+
+/*
+add a new student. whats the most important data? what key names should we use?
+
+"name" => $name,
+"slogan" => $slogan,
+"link" => $link,
+
+*/ 
+
+
