0a9dee9df68fe27b6415b99f7b7fcca742eea5ca
Marco R <mprizzuto88@gmail.com>
2021-08-23 22:08:09 -0400

    fix improper HTML syntax

--- index.php
+++ index.php
@@ -1,29 +1,14 @@
 <!doctype html>
 <html lang="en">
 	<head>
-		<meta charset="UTF-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-		<title></title>
-		<meta
-			name="description"
-			content="[[meta-description]]"
-		/>
-		<meta
-			name="og:description"
-			content="[[meta-description]]"
-		/>
-		<!-- these are self closing, no /> needed! -->
+		<meta charset="UTF-8" >
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>alpha 1 homepage</title>
+		<meta name="description" content="[[meta-description]]">
 
-		<!-- I think most of these meta tags can be removed, except the description. thoughts? -->
+		<meta property="og:image" content="[[meta-image]]">
 
-		<meta property="og:title" content="[[meta-content]]" />
-		<meta property="og:type" content="">
-		<meta property="og:url" content="">
-
-		<meta property="og:image" content="[[meta-image]]" />
-		<link rel="shortcut icon" type="image/jpg" href="[[favicon]]" />
-
-		<link rel="stylesheet" href="css/styles.css" />
+		<link rel="stylesheet" href="css/styles.css">
 	</head>
 	<body>
 		<?php 
@@ -37,7 +22,6 @@
 				<?php 
 				studentCardGenerator("marco", "lets git it done", "https://perpetual.education");
 				
-				// just a test!
 				// studentCardGenerator("alex", "web design is fun", "https://perpetual.education");
 				?>
 					
