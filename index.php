d0cbf7fbf79402f3624416fc6746e1a42a319e6d
Marco R <mprizzuto88@gmail.com>
2021-08-27 21:39:00 -0400

    create index.php file. write meta data

---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    
+    <meta charset="utf-8">
+
+    <title>alpha 1 homepage</title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <meta name="description" content="The only place on the internet to see all the alpha-1 projects.">
+
+    <meta property="og:image" content="images/a1-placeholder">
+  </head>
+  <body>  
+
+  </body>
+</html>
