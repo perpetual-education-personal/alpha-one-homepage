ff51befa549e49b555d4454da26acbada2f79d63
Thomas <metommylee@gmail.com>
2021-10-26 18:43:14 -0700

    updates on landing section

--- tommylee/index-header.php
+++ tommylee/index-header.php
@@ -1,15 +1,18 @@
 	<header class="index-header" id="home">
 		<noise-wrapper>
 			<inner-column>
-				<h1 class="loud-voice header-title">Shopify Development</h1>
-			</inner-column>
-
-			<inner-column class="header-buttons">
-				<p class="calm-voice title-text">Hi! People call me Tommy. I help businesses build and optimize their Shopify store with custom coding according to their needs. — <em>Hablemos.&nbsp;연락주세요.</em></p>
+				<div class="header-title">
+					<h1 class="loud-voice">Shopify Development</h1>
+				</div>
 				
-				<div>
-					<a href="contact.html" target="_blank" class="button-primary">Contact Now</a>
-					<a href="#values" class="button-secondary">Read More</a>
+				<div class="header-buttons">
+					<p class="calm-voice title-text">Hi! People call me Tommy. I help businesses build and optimize their Shopify store with custom coding according to their needs. — <em>Hablemos.&nbsp;연락주세요.</em></p>
+					
+					<div>
+						<a href="contact.html" target="_blank" class="button-primary">Contact&nbsp;Now</a>
+						<a href="#values" class="button-secondary">Read&nbsp;More</a>
+					</div>
+
 				</div>
 			</inner-column>			
 		</noise-wrapper>
